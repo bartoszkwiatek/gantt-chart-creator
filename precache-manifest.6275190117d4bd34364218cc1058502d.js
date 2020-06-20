7a9f93b32a12a4d06d8386f0c93db4a3e440ba14
kwiaciu <bkwiatek3@gmail.com>
2020-06-20 15:36:56 +0200

    Updates

---
+++ precache-manifest.6275190117d4bd34364218cc1058502d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93a172fb844c2578452687adef67e57",
+    "url": "/GanttChartCreator/index.html"
+  },
+  {
+    "revision": "0b349f081fee640eba1a",
+    "url": "/GanttChartCreator/static/css/main.4ccf7fd9.chunk.css"
+  },
+  {
+    "revision": "ed22eed7d7b4f8d8c8ad",
+    "url": "/GanttChartCreator/static/js/2.aaac4057.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/GanttChartCreator/static/js/2.aaac4057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b349f081fee640eba1a",
+    "url": "/GanttChartCreator/static/js/main.05e20385.chunk.js"
+  },
+  {
+    "revision": "ff1c83de062caf88421d",
+    "url": "/GanttChartCreator/static/js/runtime-main.dfe7a9a4.js"
+  }
+]);
